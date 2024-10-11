a1b7b3d13af2057c9ac78da372bbc955033950c9
55623 <A111222021@mail.shu.edu.tw>
2024-10-11 09:46:58 +0800

    113-10-11

--- 113-10-04/DecimalDemo2.java
+++ 113-10-04/DecimalDemo2.java
@@ -8,8 +8,16 @@ public class DecimalDemo2 {
         var op2 = new BigDecimal("0.1");
         var op3 = new BigDecimal("0.1");
         var result = new BigDecimal("0.3");
+        //若要比較浮點數，用BigDecimal
         
-        if(op1.add(op2).add(op3).equals(result)) {
+        if(op1.add(op2).add(op3).equals(result)) { //參考型態不要用 == ，用.equals`
+            System.out.println("等於 0.3");
+        }
+        else {
+            System.out.println("不等於 0.3");
+        }
+
+        if(op1.add(op2).add(op3).compareTo(result) == 0) { //參考型態不要用 == ，用.equals`
             System.out.println("等於 0.3");
         }
         else {
